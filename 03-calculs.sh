6d75d43fd2201e58b157e61a60f2998d831c9489
Thomas Rodriguez <thomas.rodriguez2701@gmail.com>
2017-04-24 17:46:03 -0400

    Done with calcul!

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+# Ce calcul fonctionne
+echo $((10+10))
+
+# Ce calcul ne fonctionne pas
+echo 10+10
+
+read -p "Nombre 1 : " nb1
+read -p "Nombre 2 : " nb2
+let resultat=$nb1+$nb2
+echo $resultat

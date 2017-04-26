fcdc32be31a37a59e6f5600c5a455bfc360b47b9
Thomas Rodriguez <thomas.rodriguez2701@gmail.com>
2017-04-26 15:48:02 -0400

    Exercice 1 terminé

--- Exercice 1.sh
+++ Exercice 1.sh
@@ -3,3 +3,14 @@
 # La ligne suivante permet de vérifier si un mot existe dans le dictionnaire
 # Il faut suffixer cet URL avec le mot entré par l'uasger (ex: word=chat)
 #wget -qO - http://dictionary.objectif8.com/exists.php?word=
+
+read -p "Entrez un mot: " mot
+
+resultat=`wget -qO -  http://dictionary.objectif8.com/exists.php?word=$mot`
+
+if test $resultat = "1"
+then
+	 echo "Le mot $mot existe."
+else
+	echo "Le mot $mot n'existe pas."
+fi

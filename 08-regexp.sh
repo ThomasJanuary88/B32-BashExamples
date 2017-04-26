2e912b3a97ad8dfe794fd47dbc378794e3901305
Thomas Rodriguez <thomas.rodriguez2701@gmail.com>
2017-04-26 15:17:08 -0400

    then fi

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+# Identique à read -p "Lettre: " lettre
+echo -n "Lettre:"
+read lettre
+
+# ~ = regexp!
+if [[ $lettres =~ ^[0-9]+$ ]]
+then
+	echo "C'est un nombre"
+fi
--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# $# = Nombre de paramètres passés au programme
+# -ge = greater or equal
+if test $# -ge 1
+then
+	if test -e $1
+	then
+		echo "Il existe"
+	fi
+fi

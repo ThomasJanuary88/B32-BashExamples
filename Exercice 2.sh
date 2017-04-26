4fd85394294fd6d0e4cdf5e64088832a243f2385
Thomas Rodriguez <thomas.rodriguez2701@gmail.com>
2017-04-26 16:10:07 -0400

    Le menu

--- Exercice 2.sh
+++ Exercice 2.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+afficherMenu(){
+	clear
+	echo "====================="
+	echo "|- EPIC v1.0.0.0.0 -|"
+	echo "---------------------"
+	sleep 1
+	echo "-d : Dictionnaire    "
+	echo "-q : Quitter         "
+	echo ""
+}
+
+choix="z"
+
+afficherMenu
+
+while test $choix != "q"
+do
+	read -p "Choissisez une option : " choix
+
+	if test $choix = "d"
+	then
+        ./Exerice \ 1.sh
+	fi
+done

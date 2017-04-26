48050436646c4675f4321c15e88a24dc81abfda4
Thomas Rodriguez <thomas.rodriguez2701@gmail.com>
2017-04-26 16:43:29 -0400

    Boucle exercice 3

--- Exercice 3.sh
+++ Exercice 3.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+faireEspace() {
+	nb=$1
+	i=0
+
+	while test $i -lt $nb
+	do
+		i=$(($i+1))
+		echo -n " "
+	done
+}
+
+for((i=0;i<30;i++))
+{
+	faireEspace $i
+	echo $i
+	sleep 1
+}
+
+clear
+faireEsapce 
+echo "BOOM"
